cb844aeb0daccafa09d227ec70e1eb62ce70b6fe
Gyrkora <giselle.gallardo0210@gmail.com>
2022-04-12 20:42:16 -0300

    Adding Gulp

---
+++ gulpfile.js
@@ -0,0 +1,15 @@
+// function tarea(done) {
+//   console.log("desde la primera tarea");
+
+//   done(); //callback
+// }
+
+// function tarea2(done) {
+//     console.log("desde la segunda tarea");
+  
+//     done(); //callback
+//   }
+
+// exports.tarea = tarea;
+// exports.tarea2 = tarea2;
+
